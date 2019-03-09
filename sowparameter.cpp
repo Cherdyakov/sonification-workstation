543d7cd928cc4c3cb58edd01d381e2e6ce8282b7
Sean <soundbysean@gmail.com>
2019-03-08 18:44:47 -0800

    SowParameter first draft.
    First draft of a SowParameter class, encapsulates functionality of the multiple members associated with a single QtSynthItem parameter.

--- sowparameter.cpp
+++ sowparameter.cpp
@@ -1,6 +1,127 @@
 #include "sowparameter.h"
 
+namespace sow {
+
 SowParameter::SowParameter(QObject *parent) : QObject(parent)
 {
 
 }
+
+
+void sow::SowParameter::setName(const QString name) {
+    if (name_ != name) {
+        name_ = name;
+        emit nameChanged();
+    }
+    qDebug(qUtf8Printable(name_));
+}
+
+QString sow::SowParameter::name() const {
+    return name_;
+}
+
+void sow::SowParameter::setType(const sow::PARAMETER type) {
+    if (type_ != type) {
+        type_ = type;
+        emit typeChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+sow::PARAMETER sow::SowParameter::type() const {
+    return type_;
+}
+
+void sow::SowParameter::setValue(const float value) {
+    if (qFuzzyCompare(value_, value)) {
+        value_ =  value;
+        emit valueChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+float sow::SowParameter::value() const {
+    return value_;
+}
+
+void sow::SowParameter::setFixed(const bool fixed) {
+    if (fixed_ != fixed) {
+        fixed_ =  fixed;
+        emit fixedChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+bool sow::SowParameter::fixed() const {
+    return fixed_;
+}
+
+void sow::SowParameter::setScaled(const bool scaled) {
+    if (scaled_ != scaled) {
+        scaled_ =  scaled;
+        emit scaledChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+bool sow::SowParameter::scaled() const {
+    return scaled_;
+}
+
+void sow::SowParameter::setScaleLo(const float scaleLo) {
+    if (qFuzzyCompare(scaleLo_, scaleLo)) {
+        scaleLo_ =  scaleLo;
+        emit scaleLoChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+float sow::SowParameter::scaleLo() const {
+    return scaleLo_;
+}
+
+void sow::SowParameter::setScaleHi(const float scaleHi) {
+    if (qFuzzyCompare(scaleHi_, scaleHi)) {
+        scaleHi_ =  scaleHi;
+        emit scaleHiChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+float sow::SowParameter::scaleHi() const {
+    return scaleHi_;
+}
+
+void sow::SowParameter::setScaleExp(const float scaleExp) {
+    if (qFuzzyCompare(scaleExp_, scaleExp)) {
+        scaleExp_ =  scaleExp;
+        emit scaleExpChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+float sow::SowParameter::scaleExp() const {
+    return scaleExp_;
+}
+
+void sow::SowParameter::setMap(const QString map) {
+    if (map_ != map) {
+        map_ = map;
+        emit nameChanged();
+        qDebug(qUtf8Printable(name_));
+
+    }
+}
+
+QString sow::SowParameter::map() const {
+    return map_;
+}
+
+} // Namespace sow.
--- sowparameter.h
+++ sowparameter.h
@@ -4,11 +4,42 @@
 #include <QObject>
 #include "sowenums.h"
 
+namespace sow {
+
 class SowParameter : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString name_ READ name WRITE setName)
+    Q_PROPERTY(PARAMETER type_ READ name WRITE setName)
+    Q_PROPERTY(float value_ READ name WRITE setName)
+    Q_PROPERTY(bool fixed_ READ name WRITE setName)
+    Q_PROPERTY(bool scaled_ READ name WRITE setName)
+    Q_PROPERTY(float scaleHi_ READ name WRITE setName)
+    Q_PROPERTY(float scaleL0_ READ name WRITE setName)
+    Q_PROPERTY(float scaleExp_ READ name WRITE setName)
+    Q_PROPERTY(QString map_ READ map WRITE setMap)
+
 public:
     explicit SowParameter(QObject *parent = nullptr);
+
+    void setName(const QString name);
+    QString name() const;
+    void setType(const PARAMETER type);
+    PARAMETER type() const;
+    void setValue(const float value);
+    float value() const;
+    void setFixed(const bool fixed);
+    bool fixed() const;
+    void setScaled(const bool scaled);
+    bool scaled() const;
+    void setScaleLo(const float scaleLo);
+    float scaleLo() const;
+    void setScaleHi(const float scaleHi);
+    float scaleHi() const;
+    void setScaleExp(const float scaleExp);
+    float scaleExp() const;
+    void setMap(const QString map);
+    QString map() const;
 
 private:
 
@@ -17,14 +48,26 @@ private:
     float value_;
     bool fixed_;
     bool scaled_;
+    float scaleLo_;
     float scaleHi_;
-    float scaleL0_;
     float scaleExp_;
     QString map_;
 
 signals:
 
+    void nameChanged();
+    void typeChanged();
+    void valueChanged();
+    void fixedChanged();
+    void scaledChanged();
+    void scaleHiChanged();
+    void scaleLoChanged();
+    void scaleExpChanged();
+    void mapChanged();
+
 public slots:
 };
+
+} // Namespace sow.
 
 #endif // SOWPARAMETER_H
