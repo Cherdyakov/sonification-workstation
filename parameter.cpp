2ad917a809df98edc7cd903c08bf4d9bfc63490f
Sean <soundbysean@gmail.com>
2019-03-10 14:02:56 -0700

    Parameter
    Added to sow namespace.  Variables added.  ProcessCommand function written.

--- parameter.cpp
+++ parameter.cpp
@@ -1,6 +1,45 @@
 #include "parameter.h"
 
+namespace sow {
+
 Parameter::Parameter(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Parameter::ProcessCommand(sow::ParameterCommand cmd)
+{
+    SowEnums::SUB_PARAMETER subParam = cmd.subParam;
+
+    switch (subParam) {
+    case SowEnums::SUB_PARAMETER::VALUE:
+        value_ = cmd.value;
+        break;
+    case SowEnums::SUB_PARAMETER::FIXED:
+        fixed_ = (cmd.value != 0.0f);
+        break;
+    case SowEnums::SUB_PARAMETER::SCALED:
+        scaled_ = (cmd.value != 0.0f);
+        break;
+    case SowEnums::SUB_PARAMETER::SCALE_LO:
+        scaleLo_ = cmd.value;
+        break;
+    case SowEnums::SUB_PARAMETER::SCALE_HI:
+        scaleHi_ = cmd.value;
+        break;
+    case SowEnums::SUB_PARAMETER::SCALE_EXP:
+        scaleExp_ = cmd.value;
+        break;
+    case SowEnums::SUB_PARAMETER::MAP:
+        map_ = cmd.mapping;
+        break;
+    }
+
+}
+
+void Parameter::onParameterChanged(sow::ParameterCommand cmd)
+{
+    commandBuffer_.push(cmd);
+}
+
+} // namespace sow
--- parameter.h
+++ parameter.h
@@ -2,6 +2,11 @@
 #define PARAMETER_H
 
 #include <QObject>
+#include "sowenums.h"
+#include "ringbuffer.h"
+#include "commands.h"
+
+namespace sow {
 
 class Parameter : public QObject
 {
@@ -9,9 +14,31 @@ class Parameter : public QObject
 public:
     explicit Parameter(QObject *parent = nullptr);
 
+
+private:
+
+    // Backing variables, used by sow
+    SowEnums::PARAMETER type_;
+    QString name_;
+    float value_;
+    bool fixed_;
+    bool scaled_;
+    float scaleLo_;
+    float scaleHi_;
+    float scaleExp_;
+    QString map_;
+
+    RingBuffer<ParameterCommand> commandBuffer_;
+
+    void ProcessCommand(ParameterCommand c);
+
 signals:
 
 public slots:
+    void onParameterChanged(ParameterCommand command);
+
 };
+
+} // namespace sow
 
 #endif // PARAMETER_H
