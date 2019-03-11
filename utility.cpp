e3c5005be2c7354a5558a471938ab4cbbf2cefb8
Sean <soundbysean@gmail.com>
2019-03-10 20:31:08 -0700

    Utilities work with QVector now

--- utility.cpp
+++ utility.cpp
@@ -4,16 +4,16 @@ namespace sow {
 
 // based on the Max "Scale" object
 // https://docs.cycling74.com/max7/maxobject/scale
-// default exp = 1 is linear
+// default exp = 1 is linear scaling
 double scale(double x, double in_low, double in_high, double out_low, double out_high, double exp)
 {
-    return ((x-in_low)/(in_high-in_low) == 0) ? out_low : (((x-in_low)/(in_high-in_low)) > 0) ? (out_low + (out_high-out_low) * pow(((x-in_low)/(in_high-in_low)),exp)) : ( out_low + (out_high-out_low) * -(pow((((-x+in_low)/(in_high-in_low))),exp)));
+    return ((x-in_low)/(in_high-in_low) == 0.0) ? out_low : (((x-in_low)/(in_high-in_low)) > 0.0) ? (out_low + (out_high-out_low) * pow(((x-in_low)/(in_high-in_low)),exp)) : ( out_low + (out_high-out_low) * -(pow((((-x+in_low)/(in_high-in_low))),exp)));
 }
 
-Frame visit_children(std::vector<QtSynthItem*> children)
+Frame visit_children(QVector<QtSynthItem *> children)
 {
     Frame frame;
-    for (unsigned int i = 0; i < children.size(); ++i)
+    for (int i = 0; i < children.size(); ++i)
     {
         QtSynthItem* gen = children[i];
         frame += gen->process();
@@ -22,7 +22,7 @@ Frame visit_children(std::vector<QtSynthItem*> children)
     return frame;
 }
 
-void insert_item_unique(QtSynthItem* synth_item, std::vector<QtSynthItem*>* items)
+void insert_item_unique(QtSynthItem* synth_item, QVector<QtSynthItem*>* items)
 {
     if(std::find(items->begin(), items->end(), synth_item) == items->end())
     {
@@ -30,28 +30,28 @@ void insert_item_unique(QtSynthItem* synth_item, std::vector<QtSynthItem*>* item
     }
 }
 
-void remove_item(QtSynthItem* synth_item, std::vector<QtSynthItem*>* items)
+void remove_item(QtSynthItem* synth_item, QVector<QtSynthItem*>* items)
 {
     items->erase(std::remove(items->begin(), items->end(), synth_item), items->end());
 }
 
-void remove_as_child(QtSynthItem* self, std::vector<QtSynthItem *> parents)
+void remove_as_child(QtSynthItem* self, QVector<QtSynthItem *> parents)
 {
-    for(unsigned int i = 0; i < parents.size(); i++) {
+    for(int i = 0; i < parents.size(); i++) {
         QtSynthItem* parent = parents[i];
         parent->removeChild(self);
     }
 }
 
-void remove_as_parent(QtSynthItem* self, std::vector<QtSynthItem*> children)
+void remove_as_parent(QtSynthItem* self, QVector<QtSynthItem*> children)
 {
-    for(unsigned int i = 0; i < children.size(); i++) {
+    for(int i = 0; i < children.size(); i++) {
         QtSynthItem* child = children[i];
         child->removeParent(self);
     }
 }
 
-bool verify_child(SowEnums::PARAMETER param, std::vector<SowEnums::PARAMETER> vec)
+bool verify_child(SowEnums::ITEM param, QVector<SowEnums::ITEM> vec)
 {
     return std::find(vec.begin(), vec.end(), param) != vec.end();
 }
