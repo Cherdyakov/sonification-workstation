81f189a4e31f7d694a1b704859a433400222b7e3
Sean <soundbysean@gmail.com>
2019-03-11 23:14:40 -0700

    Dataset class implementation work
    Progress on Dataset.

--- dataset.cpp
+++ dataset.cpp
@@ -8,6 +8,99 @@ Dataset::Dataset(QObject *parent) : QObject (parent)
     width_ = 0;
 }
 
+float Dataset::operator()(unsigned int x, unsigned int y)
+{
+    if ( x > height_ || y > width_ ) {
+        // throw an exception?
+    }
+    int idx = static_cast<int>((y * height_) + x);
+    return data_[idx];
+}
+
+unsigned int Dataset::height() const
+{
+    return height_;
+}
+
+unsigned int Dataset::width() const
+{
+    return width_;
+}
+
+void Dataset::initialize(const QVector<float> data, const unsigned int height, const unsigned int width)
+{
+    data_.clear();
+    data_ = data;
+    height_ = height;
+    width_ = width;
+    calculateMinMax();
+}
+
+QVector<float> Dataset::getColumn(unsigned int col) const {
+    QVector<float> vec;
+    if(col > width_)
+    {
+        return vec;
+    }
+    for(unsigned int i = 0; i < height_; i++)
+    {
+        unsigned int idx = ((width_ * i) + col);
+        vec.append(data_[static_cast<int>(idx)]);
+    }
+    return vec;
+}
+
+QVector<float> Dataset::getRow(unsigned int row) const {
+    QVector<float> vec;
+    if(row > height_)
+    {
+        return vec;
+    }
+    for(unsigned int i = 0; i < width_; i++)
+    {
+        unsigned int idx = ((width_ * row) + i);
+        vec.append(data_[static_cast<int>(idx)]);
+    }
+    return vec;
+}
+
+void Dataset::calculateMinMax()
+{
+    float min = 0.0f;
+    float max = 0.0f;
+    mins_.clear();
+    maxes_.clear();
+    for(unsigned int i = 0; i < width_; i++)
+    {
+        for(unsigned int j = 0; j < height_; j++)
+        {
+            unsigned int idx = i * height_ + j;
+            float value = data_[static_cast<int>(idx)];
+            if(j == 0)
+            {
+                min = max = value;
+            }
+            else if(value < min)
+            {
+                min = value;
+            }
+            else if(value > max)
+            {
+                max = value;
+            }
+        }
+        mins_.push_back(min);
+        maxes_.push_back(max);
+    }
+}
+
+void Dataset::clear()
+{
+    data_.clear();
+    height_ = 0;
+    width_ = 0;
+}
+
 
 
 }
--- dataset.h
+++ dataset.h
@@ -14,94 +14,29 @@ class Dataset : public QObject
 
 public:
 
+    explicit Dataset(QObject* parent = nullptr);
+
+    float operator()(unsigned int x, unsigned int y);
+    unsigned int height() const;
+    unsigned int width() const;
+    QVector<float> getColumn(unsigned int col) const;
+    QVector<float> getRow(unsigned int row) const;
+    // populate the dataset with data
+    void initialize(const QVector<float> data, const unsigned int height, const unsigned int width);
+    // erase all data and reset height, width
+    void clear();
+
 private:
 
-    uint height_;
-    uint width_;
-    QVector<double> data_;
-    QVector<double> mins_;
-    QVector<double> maxes_;
+    unsigned int height_;
+    unsigned int width_;
+    QVector<float> data_;
+    QVector<float> mins_;
+    QVector<float> maxes_;
 
-    explicit Dataset(QObject* parent = nullptr)
-    {
-        height_ = 0;
-        width_ = 0;
-    }
-
-    // helper functions
-    void init(QVector<double> data, unsigned int height, unsigned int width)
-    {
-        data_.clear();
-        data_ = data;
-        height_ = height;
-        width_ = width;
-        calculateMinMax();
-    }
-
-    QVector<double> getColumn(unsigned int col) {
-        QVector<double> vec;
-        if(col > width_)
-        {
-            return vec;
-        }
-        for(unsigned int i = 0; i < height_; i++)
-        {
-            unsigned int idx = ((width_ * i) + col);
-            vec.append(data_[idx]);
-        }
-        return vec;
-    }
-
-    QVector<double> getRow(unsigned int row) {
-        QVector<double> vec;
-        if(row > height_)
-        {
-            return vec;
-        }
-        for(unsigned int i = 0; i < width_; i++)
-        {
-            unsigned int idx = ((width_ * row) + i);
-            vec.append(data_[idx]);
-        }
-        return vec;
-    }
-
-    void calculateMinMax()
-    {
-        double min;
-        double max;
-        mins_.clear();
-        maxes_.clear();
-        for(unsigned int i = 0; i < height_; i++)
-        {
-            for(unsigned int j = 0; j < width_; j++)
-            {
-                unsigned int idx = i * width_ + j;
-                double value = data_.at(idx);
-                if(j == 0)
-                {
-                    min = max = value;
-                }
-                else if(value < min)
-                {
-                    min = value;
-                }
-                else if(value > max)
-                {
-                    max = value;
-                }
-            }
-            mins_.push_back(min);
-            maxes_.push_back(max);
-        }
-    }
-
-    void clear()
-    {
-        data_.clear();
-        height_ = 0;
-        width_ = 0;
-    }
+    // Find min/max values for every
+    // column and store them.
+    void calculateMinMax();
 
 };
 
