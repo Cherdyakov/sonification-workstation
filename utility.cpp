1e4ea497ce06d1ce6054769de21cbfc5f2133d9e
Sean <soundbysean@gmail.com>
2019-03-11 20:23:24 -0700

    Remove scaling from Utility

--- utility.cpp
+++ utility.cpp
@@ -3,14 +3,6 @@
 
 namespace sow {
 
-// based on the Max "Scale" object
-// https://docs.cycling74.com/max7/maxobject/scale
-// default exp = 1 is linear scaling
-double scale(double x, double in_low, double in_high, double out_low, double out_high, double exp)
-{
-    return ((x-in_low)/(in_high-in_low) == 0.0) ? out_low : (((x-in_low)/(in_high-in_low)) > 0.0) ? (out_low + (out_high-out_low) * pow(((x-in_low)/(in_high-in_low)),exp)) : ( out_low + (out_high-out_low) * -(pow((((-x+in_low)/(in_high-in_low))),exp)));
-}
-
 //Frame visitChildren(QVector<QtSynthItem *> children)
 //{
 //    Frame frame;
