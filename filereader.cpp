4a62f0fff0f6490f17f607c95068d3cdd7cd006f
Sean <soundbysean@gmail.com>
2019-03-12 22:34:16 -0700

    FileReader builds again
    Double > Float, other changes to match the updated Dataset class.

--- filereader.cpp
+++ filereader.cpp
@@ -1,16 +1,10 @@
 #include "filereader.h"
-#include <QTime>
 
-FileReader::FileReader(QObject *parent) : QObject(parent)
+FileReader::FileReader(QObject *parent) : QObject(parent) { }
+
+void FileReader::readCSV(const QString filename, sow::Dataset * const dataset)
 {
-
-}
-
-void FileReader::readCSV(QString filename, sow::Dataset* dataset)
-{
-    qDebug() << "Reading file: " << QTime::currentTime();
-
-    QVector<double> vec;
+    QVector<float> vec;
 
     QFile file(filename);
     if (!file.open(QFile::ReadOnly)) {
@@ -26,36 +20,36 @@ void FileReader::readCSV(QString filename, sow::Dataset* dataset)
         readData.append(line.split(","));
     }
 
-    uint height = static_cast<uint>(readData.count());   // == rows in CSV
-    uint width = static_cast<uint>(readData[0].count()); // == columns in CSV
+    int rows = readData.count();  // == rows in CSV
+    int cols = readData[0].count(); // == columns in CSV
 
     // Check if rows are of equal length
     bool unEven = false;
     for(QStringList list : readData)
     {
-        uint currentCount = static_cast<uint>(list.count());
-        if(currentCount != width)
+        int currentCount = static_cast<int>(list.count());
+        if(currentCount != cols)
         {
             unEven = true;
-            if(width < currentCount)
+            if(cols < currentCount)
             {
-                width = currentCount;
+                cols = currentCount;
             }
         }
     }
     // Some rows were shorter than others
-    // force data to have equal width rows
+    // force data to have equal cols rows
     if(unEven)
     {
-        for(uint i = 0; i < height; i++)
+        for(int i = 0; i < rows; i++)
         {
             QStringList* list = &readData[static_cast<int>(i)];
-            uint currentCount = static_cast<uint>(list->count());
+            int currentCount = static_cast<int>(list->count());
 
-            if(currentCount < width)
+            if(currentCount < cols)
             {
                 qDebug() << "Row " << i << "is too short.  Padding with 0";
-                while(currentCount < width)
+                while(currentCount < cols)
                 {
                     list->append("0");
                     currentCount++;
@@ -64,18 +58,18 @@ void FileReader::readCSV(QString filename, sow::Dataset* dataset)
         }
     }
 
-    vec.resize(static_cast<int>(height)*static_cast<int>(width));
+    vec.resize(static_cast<int>(rows)*static_cast<int>(cols));
     int index = 0;
 
-    for(uint i = 0; i < height; i++)
+    for(int i = 0; i < rows; i++)
     {
         QStringList rowData = readData[static_cast<int>(i)];
-        for (uint j = 0; j < width; j++)
+        for (int j = 0; j < cols; j++)
         {
-            bool isDouble = false;
+            bool isFloat = false;
             QString temp = rowData[static_cast<int>(j)];
-            double value = temp.toDouble(&isDouble);
-            if(isDouble)
+            float value = temp.toFloat(&isFloat);
+            if(isFloat)
             {
                 vec[index++] = value;
             }
@@ -88,14 +82,12 @@ void FileReader::readCSV(QString filename, sow::Dataset* dataset)
         }
     }
 
-    qDebug() << "Done reading file: " << QTime::currentTime();
-
-    dataset->init(vec, height, width);
+    dataset->init(vec, rows, cols);
     emit datasetChanged(dataset);
-    emit qmlDatasetChanged(dataset->height_, dataset->width_);
+    emit qmlDatasetChanged(dataset->rows(), dataset->cols());
 }
 
-void FileReader::on_newDatafile(QString filename, sow::Dataset *dataset)
+void FileReader::on_newDatafile(const QString filename, sow::Dataset * const dataset)
 {
     // clear the dataset
     if(filename.isEmpty()) {
