8547a8605ef7095a93b1a8b69545137dff3b0e2b
Sean <soundbysean@gmail.com>
2019-03-22 23:35:09 -0700

    Map Evaluator calculates min and max for given expression.
    Will allow for scaling, even when mapping an expression instead of mapping a column directly.

--- mapevaluator.h
+++ mapevaluator.h
@@ -50,6 +50,7 @@ private:
     exprtkExpression* expression_ = nullptr;
     exprtkParser parser_;
     std::vector<MapVariable<T>> currentVariables_;
+    std::string currentExpressionString_;
 
     std::vector<std::string> extractVariables(std::string expression);
     std::vector<MapVariable<T>> createVariables(const std::string expression);
@@ -86,51 +87,70 @@ template<class T>
 T MapEvaluator<T>::scale(T x, T inLow, T inHigh, T outLow, T outHigh, T exp)
 {
     return ((x-inLow)/(inHigh-inLow) == 0.0) ? outLow :
-           (((x-inLow)/(inHigh-inLow)) > 0.0) ?
-           (outLow + (outHigh-outLow) * pow(((x-inLow)/(inHigh-inLow)),exp)) :
-           (outLow + (outHigh-outLow) * -(pow((((-x+inLow)/(inHigh-inLow))),exp)));
+                                               (((x-inLow)/(inHigh-inLow)) > 0.0) ?
+                                                   (outLow + (outHigh-outLow) * pow(((x-inLow)/(inHigh-inLow)),exp)) :
+                                                   (outLow + (outHigh-outLow) * -(pow((((-x+inLow)/(inHigh-inLow))),exp)));
 }
 
 template<class T>
 T MapEvaluator<T>::scaledValue(T outLow, T outHigh)
 {
-//    return scale(value(), );
+    //    return scale(value(), );
 }
 
 template<class T>
 void MapEvaluator<T>::calculateMinMax()
 {
-    // Create new table for min/max test vars.
-    if(minMaxTable_ != nullptr) {
-        delete minMaxTable_;
-    }
-    minMaxTable_ = new exprtkSymbolTable;
-
-    // Get first row of dataset and populate table.
-    std::vector<T> row = dataset_->getRow(0);
-    for(size_t i = 0; i < row.size(); i++) {
-        symbolTable_->add_variable(std::to_string(i), row[i]);
+    // If there is no data, expression is constant
+    // and min = max = expression value.
+    if(!dataset_->hasData()) {
+        expressionMin_ = expressionMax_ = value();
+        return;
     }
 
-    // Set initial min/max values from first row.
-    expressionMin_ = expressionMax_ = expression_->value();
+    exprtkExpression minMaxExpression;
+    exprtkSymbolTable minMaxSymbolTable;
+    exprtkParser minMaxParser;
+    std::vector<MapVariable<T>> minMaxVariables;
+    std::vector<T> minMaxCurrentData = dataset_->getRow(0);
 
-    // Calculate min and max value for entire dataset with
-    // the current expression.
+    minMaxVariables = createVariables(currentExpressionString_);
+
+    // Get index values for the variables names;
+    for (MapVariable<T>& var : minMaxVariables) {
+        var.idx = utility::alphaToInt(var.alpha);
+        var.value = minMaxCurrentData.at(var.idx);
+        minMaxSymbolTable.add_variable(var.alpha, var.value);
+    }
+    minMaxSymbolTable.add_constants();
+    minMaxExpression.register_symbol_table(minMaxSymbolTable);
+    // Attempt compilation and return success.
+    bool success = minMaxParser.compile(currentExpressionString_, minMaxExpression);
+
+    expressionMin_ = expressionMax_ = minMaxExpression.value();
+
     for(size_t i = 1; i < dataset_->rows(); i++) {
-        row = dataset_->getRow(i);
-        float val = expression_->value();
-        if(val > expressionMax_) {
-            expressionMax_ = val;
-        } else if (val < expressionMin_) {
+        minMaxCurrentData = dataset_->getRow(i);
+        // Update variable values.
+        for(MapVariable<T>& var : minMaxVariables) {
+            var.value = minMaxCurrentData.at(var.idx);
+        }
+        // Re-evaluate current expression.
+        float val = minMaxExpression.value();
+        if(val < expressionMin_) {
             expressionMin_ = val;
+        } else if (val > expressionMax_) {
+            expressionMax_ = val;
         }
     }
+
     qDebug() << "Min: " << expressionMin_ << "Max: " << expressionMax_;
 }
 
 template<class T>
 bool MapEvaluator<T>::compileExpression(const std::string expressionStr) {
+
+    currentExpressionString_ = expressionStr;
 
     if(expression_ != nullptr) {
         delete expression_;
@@ -142,7 +162,7 @@ bool MapEvaluator<T>::compileExpression(const std::string expressionStr) {
     expression_ = new exprtkExpression;
     symbolTable_ = new exprtkSymbolTable;
 
-    currentVariables_ = createVariables(expressionStr);
+    currentVariables_ = createVariables(currentExpressionString_);
 
     // Can't have more variables than there are data columns.
     if(currentVariables_.size() > (currentData_ ? currentData_->size() : 0)) return false;
@@ -166,7 +186,7 @@ bool MapEvaluator<T>::compileExpression(const std::string expressionStr) {
     symbolTable_->add_constants();
     expression_->register_symbol_table(*symbolTable_);
     // Attempt compilation and return success.
-    bool success = parser_.compile(expressionStr, *expression_);
+    bool success = parser_.compile(currentExpressionString_, *expression_);
 
     if(success) {
         calculateMinMax();
@@ -176,7 +196,7 @@ bool MapEvaluator<T>::compileExpression(const std::string expressionStr) {
 }
 
 template<class T>
-bool MapEvaluator<T>::testCompileExpression(const std::string expressionStr, const std::vector<T> * const data) {
+bool MapEvaluator<T>::testCompileExpression(const std::string expressionStr) {
 
     exprtkSymbolTable testTable;
     exprtkExpression testExpression;
@@ -186,7 +206,7 @@ bool MapEvaluator<T>::testCompileExpression(const std::string expressionStr, con
     testVariables = createVariables(expressionStr);
 
     // Can't have more variables than there are data columns.
-    if(testVariables.size() > (data ? data->size() : 0)) return false;
+    if(testVariables.size() > (currentData_ ? currentData_->size() : 0)) return false;
 
     // Get index values for the variables names;
     for (MapVariable<T>& var : testVariables) {
@@ -195,12 +215,12 @@ bool MapEvaluator<T>::testCompileExpression(const std::string expressionStr, con
 
     // Check that all indexes are valid.
     for (MapVariable<T>& var : testVariables) {
-        if (var.idx > data->size() - 1) return false;
+        if (var.idx > currentData_->size() - 1) return false;
     }
 
     // Get data values from indexes and add vars to table.
     for (MapVariable<T>& var : testVariables) {
-        var.value = data->at(var.idx);
+        var.value = currentData_->at(var.idx);
         testTable.add_variable(var.alpha, var.value);
     }
 
