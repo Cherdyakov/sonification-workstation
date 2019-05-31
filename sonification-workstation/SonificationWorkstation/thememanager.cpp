734a6041cce1b6884ab78fee456aff8c38ec8cdd
Sean Phillips <soundbysean@gmail.com>
2019-05-30 22:15:34 -0700

    QML property bindings for all SynthItem colors in ThemeManager CPP class

--- sonification-workstation/SonificationWorkstation/thememanager.cpp
+++ sonification-workstation/SonificationWorkstation/thememanager.cpp
@@ -7,17 +7,147 @@ ThemeManager::ThemeManager(QObject *parent) : QObject(parent)
     oscColor_ = "#FFFFFF";
 }
 
+void ThemeManager::setOscColor(const QString &oscColor)
+{
+    if(oscColor_ != oscColor) {
+        oscColor_ = oscColor;
+        emit oscColorChanged();
+    }
+}
+
 QString ThemeManager::oscColor()
 {
     return oscColor_;
 }
 
-void ThemeManager::setOscColor(const QString &oscColor)
+void ThemeManager::setAmColor(const QString &amColor)
 {
-    if(oscColor_ != oscColor) {
-        oscColor_ = oscColor;
+    if(amColor_ != amColor) {
+        amColor_ = amColor;
+        emit amColorChanged();
     }
-    emit oscColorChanged();
+}
+
+QString ThemeManager::amColor()
+{
+    return amColor_;
+}
+
+void ThemeManager::setFmColor(const QString &fmColor)
+{
+    if(fmColor_ != fmColor) {
+        fmColor_ = fmColor;
+        emit fmColorChanged();
+    }
+}
+
+QString ThemeManager::fmColor()
+{
+    return fmColor_;
+}
+
+void ThemeManager::setAudColor(const QString &audColor)
+{
+    if(audColor_ != audColor) {
+        audColor_ = audColor;
+        emit fmColorChanged();
+    }
+}
+
+QString ThemeManager::audColor()
+{
+    return audColor_;
+}
+
+void ThemeManager::setPanColor(const QString &panColor)
+{
+    if(panColor_ != panColor) {
+        panColor_ = panColor;
+        emit panColorChanged();
+    }
+}
+
+QString ThemeManager::panColor()
+{
+    return panColor_;
+}
+
+void ThemeManager::setEnvColor(const QString &envColor)
+{
+    if(envColor_ != envColor) {
+        envColor_ = envColor;
+        emit envColorChanged();
+    }
+}
+
+QString ThemeManager::envColor()
+{
+    return envColor_;
+}
+
+void ThemeManager::setVolColor(const QString &volColor)
+{
+    if(volColor_ != volColor) {
+        volColor_ = volColor;
+        emit volColorChanged();
+    }
+}
+
+QString ThemeManager::volColor()
+{
+    return volColor_;
+}
+
+void ThemeManager::setEqColor(const QString &eqColor)
+{
+    if(eqColor_ != eqColor) {
+        eqColor_ = eqColor;
+        emit eqColorChanged();
+    }
+}
+
+QString ThemeManager::eqColor()
+{
+    return eqColor_;
+}
+
+void ThemeManager::setNseColor(const QString &nseColor)
+{
+    if(nseColor_ != nseColor) {
+        nseColor_ = nseColor;
+        emit nseColorChanged();
+    }
+}
+
+QString ThemeManager::nseColor()
+{
+    return nseColor_;
+}
+
+void ThemeManager::setOutColor(const QString &outColor)
+{
+    if(outColor_ != outColor) {
+        outColor_ = outColor;
+        emit outColorChanged();
+    }
+}
+
+QString ThemeManager::outColor()
+{
+    return outColor_;
+}
+
+void ThemeManager::setTextColor(const QString &textColor)
+{
+    if(textColor_ != textColor) {
+        textColor_ = textColor;
+        emit textColorChanged();
+    }
+}
+
+QString ThemeManager::textColor()
+{
+    return textColor_;
 }
 
 } // End namespace sow.
--- sonification-workstation/SonificationWorkstation/thememanager.h
+++ sonification-workstation/SonificationWorkstation/thememanager.h
@@ -9,21 +9,72 @@ class ThemeManager : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString oscColor READ oscColor WRITE setOscColor NOTIFY oscColorChanged)
+    Q_PROPERTY(QString amColor READ amColor WRITE setAmColor NOTIFY amColorChanged)
+    Q_PROPERTY(QString fmColor READ fmColor WRITE setFmColorr NOTIFY fmColorChanged)
+    Q_PROPERTY(QString audColor READ audColor WRITE setAudColor NOTIFY audColorChanged)
+    Q_PROPERTY(QString panColor READ panColor WRITE setPanColor NOTIFY panColorChanged)
+    Q_PROPERTY(QString envColor READ envColor WRITE setEnvColor NOTIFY envColorChanged)
+    Q_PROPERTY(QString volColor READ volColor WRITE setVolColor NOTIFY volColorChanged)
+    Q_PROPERTY(QString nseColor READ nseColor WRITE setNseColor NOTIFY nseColorChanged)
+    Q_PROPERTY(QString eqColor READ eqColor WRITE setEqColor NOTIFY eqColorChanged)
+    Q_PROPERTY(QString outColor READ outColor WRITE setOutColor NOTIFY outColorChanged)
+    Q_PROPERTY(QString textColor READ textColor WRITE setTextColor NOTIFY textColorChanged)
 
 public:
     explicit ThemeManager(QObject *parent = nullptr);
 
-    QString oscColor();
+    // QL property binding functions
     void setOscColor(const QString &oscColor);
+    QString oscColor();
+    void setAmColor(const QString &amColor);
+    QString amColor();
+    void setFmColor(const QString &fmColor);
+    QString fmColor();
+    void setAudColor(const QString &audColor);
+    QString audColor();
+    void setPanColor(const QString &panColor);
+    QString panColor();
+    void setEnvColor(const QString &envColor);
+    QString envColor();
+    void setVolColor(const QString &envColor);
+    QString volColor();
+    void setEqColor(const QString &envColor);
+    QString eqColor();
+    void setNseColor(const QString &envColor);
+    QString nseColor();
+    void setOutColor(const QString &envColor);
+    QString outColor();
+    void setTextColor(const QString &envColor);
+    QString textColor();
 
 private:
 
     QString oscColor_;
+    QString amColor_;
+    QString fmColor_;
+    QString audColor_;
+    QString panColor_;
+    QString envColor_;
+    QString volColor_;
+    QString nseColor_;
+    QString eqColor_;
+    QString outColor_;
+    QString textColor_;
 
 signals:
 
     // Notify signals
     void oscColorChanged();
+    void amColorChanged();
+    void fmColorChanged();
+    void audColorChanged();
+    void panColorChanged();
+    void envColorChanged();
+    void volColorChanged();
+    void nseColorChanged();
+    void eqColorChanged();
+    void outColorChanged();
+    void textColorChanged();
 
 public slots:
 };
