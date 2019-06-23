091420a6b8b7fe08c82e64741e64218b7a3c5c04
Sean Phillips <soundbysean@gmail.com>
2019-06-23 15:13:21 -0700

    Empty about message box shows when selected from menu.

---
+++ sonification-workstation/SonificationWorkstation/aboutmessage.cpp
@@ -0,0 +1,6 @@
+#include "aboutmessage.h"
+
+AboutMessage::AboutMessage()
+{
+
+}
---
+++ sonification-workstation/SonificationWorkstation/aboutmessage.h
@@ -0,0 +1,12 @@
+#ifndef ABOUTMESSAGE_H
+#define ABOUTMESSAGE_H
+
+#include <QObject>
+
+class AboutMessage : public QMessageBox
+{
+public:
+    AboutMessage();
+};
+
+#endif // ABOUTMESSAGE_H
