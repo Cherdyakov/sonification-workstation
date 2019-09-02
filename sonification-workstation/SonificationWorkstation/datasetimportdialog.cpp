d609af486946656190c626989d284f59a7538068
Sean Phillips <soundbysean@gmail.com>
2019-09-02 12:57:06 -0700

    Dialog sized to table, table set to 10x10 with headers A - J

--- sonification-workstation/SonificationWorkstation/datasetimportdialog.cpp
+++ sonification-workstation/SonificationWorkstation/datasetimportdialog.cpp
@@ -7,23 +7,24 @@ DatasetImportDialog::DatasetImportDialog(QString path)
     // Set dialog properties.
     setModal(true);
 
-    // Create buttons and set button roles.
+    // Create buttons and connect signals.
     buttonBox_ = new QDialogButtonBox(QDialogButtonBox::Ok |
                                       QDialogButtonBox::Cancel);
-
     connect(buttonBox_, &QDialogButtonBox::accepted,
             this, &DatasetImportDialog::onAccepted);
     connect(buttonBox_, &QDialogButtonBox::rejected,
             this, &DatasetImportDialog::onRejected);
 
+    // TableWidget setup.
+    table_ = new QTableWidget(this);
+    table_->setRowCount(10);
+    table_->setColumnCount(10);
+    table_->setHorizontalHeaderLabels({ "A", "B", "C", "D", "E",
+                                        "F", "G", "H", "I", "J" });
+
     // Get the CSV preview data and add to QTableWidget for preview.
     FileReader reader;
     QList<QStringList> dataPreview = reader.previewCSV(path);
-
-    table_ = new QTableWidget(this);
-    table_->setRowCount(dataPreview.count());
-    table_->setColumnCount(dataPreview[0].count());
-
     for(int i = 0; i < dataPreview.count(); i++)
     {
         for(int j = 0; j < dataPreview[0].count(); j++) {
@@ -32,13 +33,15 @@ DatasetImportDialog::DatasetImportDialog(QString path)
         }
     }
 
-    // Layout for dialog box.
+    // Main layout for this dialog box.
     QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    table_->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
 
-    // Add widgets
+    // Add widgets to main layout.
     mainLayout->addWidget(table_);
     mainLayout->addWidget(buttonBox_);
     setLayout(mainLayout);
+    this->adjustSize();
 }
 
 void DatasetImportDialog::onAccepted()
