768b7e0c71700d7bd8620941ef5a963ef2c6f12d
Sean Phillips <soundbysean@gmail.com>
2021-02-15 10:07:27 -0800

    Unimplemented basic interfact

--- sonification-workstation/SonificationWorkstation/dataprocessor.cpp
+++ sonification-workstation/SonificationWorkstation/dataprocessor.cpp
@@ -4,3 +4,18 @@ DataProcessor::DataProcessor(QObject *parent) : QObject(parent)
 {
 
 }
+
+std::vector<float> DataProcessor::getData(uint idx)
+{
+
+}
+
+void DataProcessor::onProcessingTypeChanged(ENUMS::PROCESSING_TYPE)
+{
+
+}
+
+void DataProcessor::onAlphaChanged(uint alpha)
+{
+
+}
